d567554969814d270be17312400bce105890a0d9
alexandrucancescu <anduhd@gmail.com>
2019-04-08 15:16:01 +0300

    clean ./dist

--- dist/model/Instance.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const InstanceSchema = new mongoose_1.Schema({});
-//# sourceMappingURL=Instance.js.map
--- dist/tests/Run.d.ts
+++
@@ -1,2 +0,0 @@
-import "./SkipsMinder";
-import "./Utils";
